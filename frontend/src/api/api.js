1d38fecdcea3193f2329f3e0e0f76c64894536e3
juliog-a <julioblak36@gmail.com>
2025-03-29 18:56:11 +0100

    parapublicar v2

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api", // URL del backend Django
+  baseURL: "https://chees-script.onrender.com/api",
 });
+
 
 export default API;
