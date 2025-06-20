ad7a9e4b3f5a73ab51cda154d5e5b6a66c409296
Julio <81182798+Juliog-a@users.noreply.github.com>
2025-06-20 18:48:29 +0200

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,8 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://chees-script.onrender.com/api",
-  // Antigua URL en local baseURL: "http://localhost:8000",
+  baseURL: "http://localhost:8000",
   withCredentials: true, 
 });
 
